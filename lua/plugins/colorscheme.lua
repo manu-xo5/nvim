fc44e7ccb572d830b2bf128dbe006cb66fa3d415
manu-xo5 <mohit.matwaya@gmail.com>
2025-03-15 17:07:26 +0530

    fix: setup theme name from single place

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,41 +1,44 @@
+local defaultConfig = function()
+	vim.cmd("colorscheme mellifluous")
+end
+
 return {
-  {
-    "askfiy/visual_studio_code",
-    priority = 100,
-    config = function()
-      require("visual_studio_code").setup({
-        mode = "dark",
-        dim_inactive = false,
-        transparent = false,
-        expand = {
-          null_ls = true,
-          nvim_cmp = true,
-          lspconfig = true,
-          telescope = true,
-          bufferline = true,
-          nvim_treesitter = true,
-          vim_visual_multi = true,
-        },
-      })
-    end,
-  },
-  {
-    "tjdevries/colorbuddy.nvim",
-    lazy = false,
-    priority = 1000,
-  },
-  {
-    "rose-pine/neovim",
-    name = "rose-pine",
-  },
-  {
-    "ramojus/mellifluous.nvim",
-    config = function()
-      require("mellifluous").setup({
-        dim_inactive = false,
-        color_set = "mountain",
-      })
-      vim.cmd("colorscheme mellifluous")
-    end,
-  }
+	{
+		"askfiy/visual_studio_code",
+		priority = 100,
+		opts = {
+			mode = "dark",
+			dim_inactive = false,
+			transparent = false,
+			expand = {
+				null_ls = true,
+				nvim_cmp = true,
+				lspconfig = true,
+				telescope = true,
+				bufferline = true,
+				nvim_treesitter = true,
+				vim_visual_multi = true,
+			},
+		},
+		config = defaultConfig,
+	},
+	{
+		"tjdevries/colorbuddy.nvim",
+		lazy = false,
+		priority = 1000,
+		config = defaultConfig,
+	},
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		config = defaultConfig,
+	},
+	{
+		"ramojus/mellifluous.nvim",
+		opts = {
+			dim_inactive = false,
+			color_set = "mountain",
+		},
+		config = defaultConfig,
+	},
 }
