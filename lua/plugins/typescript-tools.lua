a84493aa05ad6048da1a6b659cafcc902f752b1e
manu-xo5 <mohit.matwaya@gmail.com>
2025-03-20 14:26:34 +0530

    fix: typescript-tools call setup

--- lua/plugins/typescript-tools.lua
+++ lua/plugins/typescript-tools.lua
@@ -1,14 +1,21 @@
 return {
-  "pmizio/typescript-tools.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "neovim/nvim-lspconfig"
-  },
-  keys = {
-    {"<leader>lR", vim.cmd.TSToolsRenameFile},
-    {"<leader>ai", vim.cmd.TSToolsAddMissingImports},
-    {"<leader>ru", vim.cmd.TSToolsRemoveUnusedImports},
-    {"<leader>oi", vim.cmd.TSToolsOrganizeImports},
-  },
+	"pmizio/typescript-tools.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"neovim/nvim-lspconfig",
+	},
+	keys = {
+		{ "<leader>lR", vim.cmd.TSToolsRenameFile },
+		{ "<leader>ai", vim.cmd.TSToolsAddMissingImports },
+		{ "<leader>ru", vim.cmd.TSToolsRemoveUnusedImports },
+		{ "<leader>oi", vim.cmd.TSToolsOrganizeImports },
+	},
+	config = function()
+		require("typescript-tools").setup({
+			on_attach = function(client)
+				client.server_capabilities.documentFormattingProvider = false
+				client.server_capabilities.documentRangeFormattingProvider = false
+			end,
+		})
+	end,
 }
-
