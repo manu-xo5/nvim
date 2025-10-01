9a1a1b539cca3b6762cab64701eb6d4cfc330bd4
manu-xo5 <mohit.matwaya@gmail.com>
2025-10-01 09:42:48 +0530

    feat: add keymap to jump between function

--- lua/plugin/treesitter.lua
+++ lua/plugin/treesitter.lua
@@ -1,20 +1,42 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  opts = {
-    ensure_installed = {
-      "c",
-      "lua",
-      "vim",
-      "vimdoc",
-      "query",
-      "html",
-      "css",
-      "javascript",
-      "typescript"
-    },
-    sync_install = false,
-    highlight = { enable = true },
-    indent = { enable = true },
-  }
+	{
+		"nvim-treesitter/nvim-treesitter",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter-textobjects",
+		},
+		lazy = false,
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = {
+					"c",
+					"lua",
+					"vim",
+					"vimdoc",
+					"query",
+					"html",
+					"css",
+					"javascript",
+					"tsx",
+					"typescript",
+				},
+				sync_install = false,
+				highlight = { enable = true },
+				indent = { enable = true },
+
+				textobjects = {
+					move = {
+						enable = true,
+						set_jumps = true,
+						goto_next_end = {
+							["]]"] = "@function.outer",
+						},
+						goto_previous_start = {
+							["[["] = "@function.outer",
+						},
+					},
+				},
+			})
+		end,
+	},
 }
